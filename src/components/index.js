3b6cb89305226df1794cda9804047f20c93cef42
Huynh Uyen <uyenhuynh13122000@gmail.com>
2023-11-28 15:22:02 +0700

    handleChange Reducer

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import FormRow from "./FormRow";
 import Navbar from "./Navbar";
 import SmallSidebar from "./SmallSidebar";
 import BigSidebar from "./BigSidebar";
+import FormRowSelect from "./FormRowSelect";
 
 export {
     Logo,
     FormRow,
     Navbar,
     SmallSidebar,
-    BigSidebar
+    BigSidebar,
+    FormRowSelect
 };
--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -1,5 +1,110 @@
+import { FormRow, FormRowSelect } from '../../components';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import { handleChange } from '../../features/job/jobSlice';
+
 const AddJob = () => {
-    return <h1>AddJob</h1>;
+    const {
+        isLoading,
+        position,
+        company,
+        jobLocation,
+        jobType,
+        jobTypeOptions,
+        status,
+        statusOptions,
+        isEditing,
+        editJobId,
+    } = useSelector((store) => store.job);
+
+    const disabled = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!position || !company || !jobLocation) {
+            toast.error('Please fill in all fields');
+            return;
+        }
+    };
+
+    const handleJobInput = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        disabled(handleChange({name, value}));
+    };
+
+    return (
+        <Wrapper>
+            <form className='form'>
+                <h3>{isEditing ? 'edit job' : 'add job'}</h3>
+
+                <div className='form-center'>
+                    {/* position */}
+                    <FormRow
+                        type='text'
+                        name='position'
+                        value={position}
+                        handleChange={handleJobInput}
+                    />
+
+                    {/* company */}
+                    <FormRow
+                        type='text'
+                        name='company'
+                        value={company}
+                        handleChange={handleJobInput}
+                    />
+
+                    {/* jobLocation */}
+                    <FormRow
+                        type='text'
+                        name='jobLocation'
+                        labelText='job location'
+                        value={jobLocation}
+                        handleChange={handleJobInput}
+                    />
+
+                    {/* status */}
+                    <FormRowSelect
+                        name='status'
+                        value={status}
+                        handleChange={handleJobInput}
+                        list={statusOptions}
+                    />
+
+                    {/* job type */}
+                    <FormRowSelect
+                        name='jobType'
+                        labelText='job type'
+                        value={jobType}
+                        handleChange={handleJobInput}
+                        list={jobTypeOptions}
+                    />
+
+                    <div className='btn-container'>
+                        <button
+                            type='button'
+                            className='btn btn-block clear-btn'
+                            onClick={() => console.log('clear values')}
+                        >
+                            clear
+                        </button>
+
+                        <button
+                            type='submit'
+                            className='btn btn-block submit-btn'
+                            onClick={handleSubmit}
+                            disabled={isLoading}
+                        >
+                            submit
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </Wrapper>
+    ) 
 };
 
 export default AddJob;
