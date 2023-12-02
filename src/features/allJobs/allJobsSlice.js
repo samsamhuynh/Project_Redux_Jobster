89617d0758473e7cd512457094bc3e4b46de1351
Huynh Uyen <uyenhuynh13122000@gmail.com>
2023-12-02 09:47:45 +0700

    Stats Page Structure

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -35,6 +35,19 @@ export const getAllJobs = createAsyncThunk(
   }
 );
 
+export const showStats = createAsyncThunk(
+  "allJobs/showStats",
+  async (_, thunkAPI) => {
+    try {
+      const resp = await customFetch.get("/jobs/stats");
+      console.log(resp.data);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
+
 const allJobsSlice = createSlice({
   name: "allJobs",
   initialState,
@@ -52,11 +65,28 @@ const allJobsSlice = createSlice({
     [getAllJobs.pending]: (state) => {
       state.isLoading = true;
     },
+
     [getAllJobs.fulfilled]: (state, { payload }) => {
       state.isLoading = false;
       state.jobs = payload.jobs;
     },
+
     [getAllJobs.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
+
+    [showStats.pending]: (state) => {
+      state.isLoading = true;
+    },
+
+    [showStats.fulfilled]: (state, { payload }) => {
+      state.isLoading = false;
+      state.stats = payload.defaultStats;
+      state.monthlyApplications = payload.monthlyApplications;
+    },
+
+    [showStats.rejected]: (state, { payload }) => {
       state.isLoading = false;
       toast.error(payload);
     },
