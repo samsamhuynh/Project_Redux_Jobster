b5e0af1b1c73afd9e60b5e8ac133f48a50cbc7f2
Huynh Uyen <uyenhuynh13122000@gmail.com>
2023-12-02 15:34:55 +0700

    Change Page Functionality

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -68,6 +68,10 @@ const allJobsSlice = createSlice({
     clearFilters: (state) => {
       return { ...state, ...initialFiltersState };
     },
+
+    changePage: (state, { payload }) => {
+      state.page = payload;
+    },
   },
 
   extraReducers: {
@@ -104,7 +108,12 @@ const allJobsSlice = createSlice({
   },
 });
 
-export const { showLoading, hideLoading, handleChange, clearFilters } =
-  allJobsSlice.actions;
+export const {
+  showLoading,
+  hideLoading,
+  handleChange,
+  clearFilters,
+  changePage,
+} = allJobsSlice.actions;
 
 export default allJobsSlice.reducer;
